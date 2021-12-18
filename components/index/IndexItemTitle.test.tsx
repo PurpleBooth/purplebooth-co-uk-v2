909fa07ff7e975f5e25da0dfd28ab41c8b0690e7
Billie Thompson <billie@billiecodes.com>
2021-12-19 00:34:01 +0100

    fix: Add first jest test

---
+++ components/index/IndexItemTitle.test.tsx
@@ -0,0 +1,38 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { render } from "@testing-library/react";
+import { IndexItemTitle } from "./IndexItemTitle";
+
+describe("IndexItemTitle", () => {
+  describe("small version", () => {
+    it("shows the tile", async () => {
+      const { getByText } = render(<IndexItemTitle pageHasTitle title={"Example Title Here"}/>)
+
+      expect(getByText("Example Title Here")).toBeInTheDocument()
+    })
+
+
+    it("is small", async () => {
+      const { getByTestId } = render(<IndexItemTitle pageHasTitle title={"Example Title Here"}/>)
+
+      expect(getByTestId("IndexItemTitle h2")).toBeInTheDocument()
+    })
+  })
+  describe("big version", () => {
+    it("shows the tile", async () => {
+      const { getByText } = render(<IndexItemTitle title={"Example Title Here"}/>)
+
+      expect(getByText("Example Title Here")).toBeInTheDocument()
+    })
+
+
+    it("is big", async () => {
+      const { getByTestId } = render(<IndexItemTitle title={"Example Title Here"}/>)
+
+      expect(getByTestId("IndexItemTitle h1")).toBeInTheDocument()
+    })
+  })
+
+})
