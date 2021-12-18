b1ea4f350a0742406b4d1c09bc1d53fa04f9847c
Billie Thompson <billie@billiecodes.com>
2021-12-19 00:13:45 +0100

    fix: Center the logo on the mobile version

--- components/nav/MobileNav.tsx
+++ components/nav/MobileNav.tsx
@@ -4,9 +4,9 @@ import mark from "../../public/Purple Booth Mark.svg";
 
 export const MobileNav = () => (
   <nav className={"lg:hidden bg-slate-100 dark:bg-slate-900"}>
-    <div className="flex inline-flex m-4 rounded-full aspect-square items-center justify-center bg-slate-100 p-1">
+    <div className="flex inline-flex m-4 rounded-full aspect-square content-center items-center justify-center bg-slate-100 p-1">
       <Link passHref href={"/"}>
-        <a className={""}>
+        <a className={"flex inline-flex"}>
           <Image
             width={49}
             height={49}
