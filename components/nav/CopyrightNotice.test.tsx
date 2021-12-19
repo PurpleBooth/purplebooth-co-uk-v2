d2ebe8133b044c919bfce7b1aa7f4110c6d60f7e
Billie Thompson <billie@billiecodes.com>
2021-12-19 21:40:09 +0100

    fix: Add test for copyright notice

---
+++ components/nav/CopyrightNotice.test.tsx
@@ -0,0 +1,18 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { render } from "@testing-library/react";
+import { CopyrightNotice } from "./CopyrightNotice";
+
+describe("CopyrightNotice", () => {
+  it("has the current year", async () => {
+    const { getByText } = render(
+      < CopyrightNotice/>
+    );
+    const currentYear = (new Date()).getFullYear().toString();
+    let re = new RegExp(currentYear);
+
+    expect(getByText(re)).toBeInTheDocument();
+  });
+});
