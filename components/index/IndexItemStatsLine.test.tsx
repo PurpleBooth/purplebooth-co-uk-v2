10be6e3f0ffc6570bdc62788ffaca67ec629c981
Billie Thompson <billie@billiecodes.com>
2021-12-19 01:07:31 +0100

    fix: Make sure we disable any internationalisation before testing

--- components/index/IndexItemStatsLine.test.tsx
+++ components/index/IndexItemStatsLine.test.tsx
@@ -9,20 +9,24 @@ import { IndexItemStatsLine } from "./IndexItemStatsLine";
 describe("IndexItemTitle", () => {
   it("internationalise the read length", async () => {
     const { getByText } = render(
-      <IndexItemStatsLine date={new Date(2000, 1, 1)} readLengthMin={23} />
+      <IndexItemStatsLine
+        date={new Date(2000, 2, 1, 12, 11, 10)}
+        readLengthMin={23}
+      />
     );
 
-    expect(getByText(/23 Min./)).toBeInTheDocument();
+    expect(getByText(/23 min/)).toBeInTheDocument();
   });
 
   it("internationalise the date", async () => {
     const { getByText } = render(
       <IndexItemStatsLine
-        date={new Date(2000, 1, 1, 12, 11, 10)}
+        date={new Date(2000, 2, 1, 12, 11, 10)}
         readLengthMin={23}
-      />
+      />,
+      {}
     );
 
-    expect(getByText(/1.2.2000/)).toBeInTheDocument();
+    expect(getByText(/3\/1\/2000/)).toBeInTheDocument();
   });
 });
