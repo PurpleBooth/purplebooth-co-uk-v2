5255831d821f8d61205df5fa0487725a86875bb7
Billie Thompson <billie@billiecodes.com>
2021-12-21 14:58:50 +0100

    fix: Add security headers

--- next.config.js
+++ next.config.js
@@ -1,5 +1,36 @@
 /** @type {import('next').NextConfig} */
 
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on'
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload'
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block'
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN'
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()'
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin'
+  }
+];
+
 const withMDX = require("@next/mdx")({
   extension: /\.mdx?$/,
   commonmark: true,
@@ -24,5 +55,14 @@ module.exports = withMDX({
         destination: '/page/1'
       }
     ];
-  }
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ]
+  },
 });
